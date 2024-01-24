3402c6fbb71523386ec049c69ed1ffb0fe60b3d9
xs-piyush <157465719+xs-piyush@users.noreply.github.com>
2024-01-24 11:16:46 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
+#------------------------Task 01 "Sum of two numbers"-----------------------------------------
 
-a=1
-echo "Enter the second number of your choice:"
-read $1
-sum=$(($a + $1))
-echo "The sum is: $sum"
+
+#a=1
+#echo "Enter the second number of your choice:"
+#read $1
+#sum=$(($a + $1))
+#echo "The sum is: $sum"
+
+
+#-------------------------Task 02: "String added will be printed"-----------------------------------------
+
+
+a='beamUser'
+name="$1"  # Access the first command-line argument
+echo "The action will be performed by $a with $name privileges"
